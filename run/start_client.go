f102a39e7fc48f4edb016be11961c4db258bf911
Oleksandr Nykytenko <alexvwan@gmail.com>
2018-06-07 19:00:35 +0300

    Improved run application to prompt for specific port

--- run/start_client.go
+++ run/start_client.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	logger "github.com/sirupsen/logrus"
 	"io"
+	"os"
 	"os/exec"
 	"runtime"
+	"strconv"
+	"strings"
 )
 
 func StartClient() {
@@ -16,7 +19,18 @@ func StartClient() {
 		extension = ".exe"
 	}
 	fmt.Println("Starting client...")
-	client := exec.Command("mini-sftp-client-"+goOS+extension, "-importPath", "mini-sftp-client", "-runMode", "prod")
+
+	readPort := bufio.NewReader(os.Stdin)
+	fmt.Print("Port to listen on: ")
+	portToListen, _ := readPort.ReadString('\n')
+	portToListen = strings.TrimSuffix(portToListen, "\n")
+	portToListen = strings.TrimSuffix(portToListen, "\r")
+
+	if _, err := strconv.Atoi(portToListen); err != nil {
+		logger.Fatalf("Port should be a number: %v", err)
+	}
+
+	client := exec.Command("mini-sftp-client-"+goOS+extension, "-importPath", "mini-sftp-client", "-runMode", "prod", "-port", portToListen)
 
 	stdout, err := client.StdoutPipe()
 	if nil != err {
