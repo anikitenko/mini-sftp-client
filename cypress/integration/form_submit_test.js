c3b7ff4f9e6be53c9f220e8e7c80a9d09dff4567
Oleksandr Nykytenko <alexvwan@gmail.com>
2018-06-08 21:23:52 +0300

    Various changes to cypress and travis-ci

--- cypress/integration/form_submit_test.js
+++ cypress/integration/form_submit_test.js
@@ -1,9 +1,9 @@
 context('MainForm', () => {
     it('Visit main page, enter data and submit form', () => {
-        cy.visit("http://127.0.0.1:9000");
-        cy.get('#sshIp').type('sftp-mock-test').should('have.value', 'sftp-mock-test');
-        cy.get("#sshUser").type('test').should('have.value', 'test');
-        cy.get("#sshPassword").type('test').should('have.value', 'test');
-        cy.get("#sshPort").type('22').should('have.value', '2222');
+        cy.visit("http://127.0.0.1:9000")
+            .get('#sshIp').type('sftp-mock-test').should('have.value', 'sftp-mock-test')
+            .get("#sshUser").type('test').should('have.value', 'test')
+            .get("#sshPassword").type('test').should('have.value', 'test')
+            .get("#sshPort").type('22').should('have.value', '2222');
     });
 });
