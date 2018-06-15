824d99222f3e40f858d41efee341fc12d0b83a75
Oleksandr Nykytenko <alexvwan@gmail.com>
2018-06-15 16:18:43 +0300

    Updates to deploy file

--- deploy.sh
+++ deploy.sh
@@ -7,17 +7,27 @@ CGO_ENABLED=0 revel build github.com/anikitenko/mini-sftp-client mini-sftp-osx
 CGO_ENABLED=0 GOOS=linux revel build github.com/anikitenko/mini-sftp-client mini-sftp-linux
 CGO_ENABLED=0 GOOS=windows revel build github.com/anikitenko/mini-sftp-client mini-sftp-windows
 
-find mini-sftp-osx/src/github.com/anikitenko/mini-sftp-client \
- -maxdepth 1 ! -path mini-sftp-osx/src/github.com/anikitenko/mini-sftp-client \
- -not -name app -not -name conf -not -name public -exec rm -rf {} +
+rm -f mini-sftp-osx/run.sh mini-sftp-osx/run.bat
+rm -f mini-sftp-linux/run.sh mini-sftp-linux/run.bat
+rm -f mini-sftp-windows/run.sh mini-sftp-windows/run.bat
 
-find mini-sftp-linux/src/github.com/anikitenko/mini-sftp-client \
- -maxdepth 1 ! -path mini-sftp-linux/src/github.com/anikitenko/mini-sftp-client \
- -not -name app -not -name conf -not -name public -exec rm -rf {} +
+find 'mini-sftp-osx/src/github.com/anikitenko/mini-sftp-client' \
+ -maxdepth 1 ! -path 'mini-sftp-osx/src/github.com/anikitenko/mini-sftp-client' \
+ -not -name 'mini-sftp-osx/src/github.com/anikitenko/mini-sftp-client/app' \
+ -not -name 'mini-sftp-osx/src/github.com/anikitenko/mini-sftp-client/conf' \
+ -not -name 'mini-sftp-osx/src/github.com/anikitenko/mini-sftp-client/public' -exec rm -rf {} +
 
-find mini-sftp-windows/src/github.com/anikitenko/mini-sftp-client \
- -maxdepth 1 ! -path mini-sftp-windows/src/github.com/anikitenko/mini-sftp-client \
- -not -name app -not -name conf -not -name public -exec rm -rf {} +
+find 'mini-sftp-linux/src/github.com/anikitenko/mini-sftp-client' \
+ -maxdepth 1 ! -path 'mini-sftp-linux/src/github.com/anikitenko/mini-sftp-client' \
+ -not -name 'mini-sftp-linux/src/github.com/anikitenko/mini-sftp-client/app' \
+ -not -name 'mini-sftp-linux/src/github.com/anikitenko/mini-sftp-client/conf' \
+ -not -name 'mini-sftp-linux/src/github.com/anikitenko/mini-sftp-client/public' -exec rm -rf {} +
+
+find 'mini-sftp-windows/src/github.com/anikitenko/mini-sftp-client' \
+ -maxdepth 1 ! -path 'mini-sftp-windows/src/github.com/anikitenko/mini-sftp-client' \
+ -not -name 'mini-sftp-linux/src/github.com/anikitenko/mini-sftp-client/app' \
+ -not -name 'mini-sftp-linux/src/github.com/anikitenko/mini-sftp-client/conf' \
+ -not -name 'mini-sftp-linux/src/github.com/anikitenko/mini-sftp-client/public' -exec rm -rf {} +
 
 CGO_ENABLED=0 go build -o ./mini-sftp-osx/run ./run && chmod +x ./mini-sftp-osx/run
 CGO_ENABLED=0 GOOS=linux go build -o ./mini-sftp-linux/run ./run && chmod +x ./mini-sftp-linux/run
